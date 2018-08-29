b575cd7eb78cc3c18fff09d170427044fb387868
Bohdan Chaban <bohdan.chaban@itv.com>
2018-08-29 12:55:52 +0300

    Add proxy and observer examples

--- patterns/observer.rb
+++ patterns/observer.rb
@@ -1,1 +1,50 @@
-# TODO
+# Observer
+class Team
+  attr_reader :name
+
+  def initialize
+    @name = 'Karpaty Lviv'
+    @managers = []
+  end
+
+  def add_player(player)
+    @player = player
+    puts "Player #{@player} is added to team"
+    notify_managers
+  end
+
+  def add_manager(manager)
+    @managers << manager
+  end
+
+  def delete_manager(manager)
+    @managers.delete(manager)
+  end
+
+  def notify_managers
+    @managers.each { |manager| manager.team_update(self) }
+  end
+end
+
+# Subscriber
+
+class Coach
+  def initialize(name)
+    @name = name
+  end
+
+  def team_update(team)
+    puts "#{@name} update: Team #{team.name} is updated"
+  end
+end
+
+team = Team.new
+
+team.add_manager(Coach.new('Jose Mourinho'))
+team.add_manager(Coach.new('Sir Alex Ferguson'))
+
+team.add_player('Marjan Shved')
+
+# Player Marjan Shved is added to team
+# Jose Mourinho update: Team Karpaty Lviv is updated
+# Sir Alex Ferguson update: Team Karpaty Lviv is updated
--- patterns/proxy.rb
+++ patterns/proxy.rb
@@ -1,1 +1,98 @@
-# TODO
+class TeamAccount
+  attr_reader :balance
+
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
+  def increase(amount)
+    @balance += amount
+  end
+
+  def decrease(amount)
+    @balance -= amount
+  end
+end
+
+class VirtualAccountProxy
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
+  def increase(amount)
+    subject.increase(amount)
+  end
+
+  def decrease(amount)
+    subject.decrease(amount)
+  end
+
+  def balance
+    subject.balance
+  end
+
+  private
+
+  def subject
+    @subject ||= TeamAccount.new(@balance)
+  end
+end
+
+class RemoteAccountProxy
+  def initialize
+    @base_uri = "localhost:3000/team_account"
+  end
+
+  def balance
+    rest_service.get("/balance")
+  end
+
+  def increase(amount)
+    rest_service.post("/increase", {amount: amount})
+  end
+
+  def decrease(amount)
+    rest_service.delete("/decrease", {amount: amount})
+  end
+
+  private
+
+  attr_reader :rest_service
+
+  def rest_service
+    @rest_client ||= RestClient.new(@base_uri, :json)
+  end
+end
+
+class ProtectionAccountProxy
+  attr_reader :user_credentials
+
+  def initialize(user_credentials)
+    @subject = TeamAccount.new
+    @user_credentials = user_credentials
+  end
+
+  def balance
+    check_permissions(:read)
+    @subject.balance
+  end
+
+  def increase(amount)
+    check_permissions(:write)
+    @subject.increase(amount)
+  end
+
+  def decrease(amount)
+    check_permissions(:write)
+    @subject.decrease(amount)
+  end
+
+  private
+
+  def check_permissions(permission_type)
+    # CredentialValidator isn't implemented in this example
+    unless CredentialValidator.validate(@user_credentials, permission_type)
+      raise "Account #{@user_credentials} #{permission_type} action denied."
+    end
+  end
+end
